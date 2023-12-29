1f7be7e9963bb7edcfca7c5cc3756916b3712acc
Shadowcatz <ngquocbinh2@gmail.com>
2023-12-29 09:49:29 +0700

    check

---
+++ assets/css/maiin.js
@@ -0,0 +1,12 @@
+// hide-show 
+
+// var btnbuy = document.querySelector('.place-item button');
+// var modal = document.querySelector('.modal');
+
+// function removehide() {
+//     modal.classList.remove("hide")
+// }
+
+
+
+// btnbuy.onclick = removehide;
